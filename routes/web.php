cb1661a568388522478bd358d411abeb00d21ffa
developerUser12-sudo <jebular481@gmail.com>
2026-03-19 10:34:04 +0100

    bug reservar

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('aviso-legal', function () {
     return view('aviso-legal');
 });
 Route::get('reservar', [ReservarCitaController::class, 'create'])->name('reservar');
-Route::post('reservar', [ReservarCitaController::class, 'reservar'])->name('reservar');
+Route::post('reservar', [ReservarCitaController::class, 'reservar']);
 Route::get('cita-confirmada/{id}', [ReservarCitaController::class, 'confirmada'])->name('cita-confirmada');
 Route::get('cita-confirmada/{id}/calendar', [ReservarCitaController::class, 'calendar'])->name('calendario');
 Route::get('/dashboard', function () {
