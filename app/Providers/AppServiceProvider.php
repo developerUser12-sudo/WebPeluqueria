aaa10c98abd358804747a8a16e46d1dfe6e07022
developerUser12-sudo <jebular481@gmail.com>
2026-03-20 12:29:11 +0100

    bug icons

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_ENV') === 'production') {
-        URL::forceScheme('https'); // <--- fuerza HTTPS en todos los enlaces y asset()
+        URL::forceScheme('https');
     }
     }
 }
