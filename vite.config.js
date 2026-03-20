790dbd182f25734caf9f3c3de4f1c8f08beb9eca
developerUser12-sudo <jebular481@gmail.com>
2026-03-20 12:45:13 +0100

    bug icons

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
     base: process.env.VITE_ASSET_URL + '/',
     plugins: [
@@ -8,5 +8,13 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'node_modules/bootstrap-icons/font/fonts/*',
+                    dest: 'assets/fonts'
+                }
+            ]
+        })
     ],
 });
