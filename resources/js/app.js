ab4442dadde9196dc5a2a72df733f07f1c7de2ce
developerUser12-sudo <jebular481@gmail.com>
2026-03-20 12:33:14 +0100

    bug icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,11 @@
 import './bootstrap';
+
+// Bootstrap CSS y JS
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import Alpine from 'alpinejs';
 
