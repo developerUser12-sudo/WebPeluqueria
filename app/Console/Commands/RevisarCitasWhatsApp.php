847bc6552c7cdd108390e6380be16935e7019cc3
developerUser12-sudo <jebular481@gmail.com>
2026-07-06 14:26:09 +0200

    debug fecha cita

--- app/Console/Commands/RevisarCitasWhatsApp.php
+++ app/Console/Commands/RevisarCitasWhatsApp.php
@@ -19,11 +19,15 @@ class RevisarCitasWhatsApp extends Command
             ->filter(function ($cita) {
 
                 $fechaCita = \Carbon\Carbon::parse($cita->dia . ' ' . $cita->hora);
-
+\Log::info('Enviando recordatorio', [
+    'ahora' => now()->toDateTimeString(),
+    'cita' => $fechaCita->toDateTimeString(),
+]);
                 return $fechaCita->between(
                     now()->addMinutes(120),
                     now()->addMinutes(150)
                 );
+                
             });
         $telefono = '';
         $nombre = '';
